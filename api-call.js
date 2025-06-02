db32bd2ba5b5ed12ac1be3405df2f218f7114a1b
Sara Elwatany <14712019100970@stud.cu.edu.eg>
2025-06-02 13:45:19 +0300

    Add Deployed API

--- api-call.js
+++ api-call.js
@@ -22,7 +22,7 @@ async function getPredictedLabel(landmarks) {
 
   try {
 
-    const response = await fetch("http://127.0.0.1:8000/predict", {
+    const response = await fetch("http://rkagrzadomxf.eu-central-1.clawcloudrun.com/predict", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
